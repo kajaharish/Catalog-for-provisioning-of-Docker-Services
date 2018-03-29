d90e3e592d4bd3c58b1c036c3c533ee1bc9f6882
Gaurav Gandhi <gauravgo4friends@gmail.com>
2018-03-30 00:55:37 +0530

    The length of the Random key generated is reduced to 20.

--- RandomKeyGenerator.py
+++ RandomKeyGenerator.py
@@ -3,10 +3,9 @@ import random
 def randomKey():
     key = ""
     i=0
-    while i<35:
-        n = random.randint(48,122)
+    while i<15:
+        n = random.randint(97,122)
         key = key + chr(n)
         i+=1
-    print key
     return key
 
