a0e6753f189ef173d31518357d3e797f2b1e6425
Gaurav Gandhi <gauravgo4friends@gmail.com>
2018-03-30 00:56:02 +0530

    Login page is updated so that it can be used to create sessions using the cookies and the sessions table.

--- login.py
+++ login.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 import cgi, cgitb
 import MySQLdb
+import RandomKeyGenerator as rkg
+import CookieGenerator as cookie
+
 cgitb.enable()
 print "Content-type:text/html\r\n\r\n"
+
 
 def connectDB():
     db = MySQLdb.connect("172.17.0.2","root","123456","minor_db")
     cursor = db.cursor()
     return (db,cursor)
+
 
 def fetchDetails():
     form = cgi.FieldStorage()
@@ -15,18 +20,65 @@ def fetchDetails():
     password = form.getvalue('password')
     return (username,password)
 
-def login(cursor,username,password):
+
+def login(db,cursor,username,password):
     try:
         #Execute the SQL command
         sql = ('select * from user where username = "%s" and password = "%s"'%(username,password))
         chk = cursor.execute(sql)
         if chk == 1L:
-            print ("Login Successfull")
+            #Generate Session id.
+            user_check = checkUser(cursor,username)
+            if user_check == 1:
+                ssn = 0
+                while ssn == 0:
+                    session_id = rkg.randomKey()
+                    ssn_chk = checkSessionId(cursor,session_id)
+                    if ssn_chk == 1:
+                        ssn = 1
+                        break
+                #Create Cookie.
+                cookie.genCookie(session_id)
+                #Update the cookie in the sessions table.
+                createSesssion(db,cursor,session_id,username)
+                print ("Login Successfull")
         else:
             print ("Please check the username or password")
     except:
         db.rollback()
         print " Not updated"
+
+
+def checkSessionId(cursorn,session_id):
+    sql = "select * from session where session_id = '%s'"%session_id
+    check = cursor.execute(sql)
+    if check == 1L:
+        return 0
+    else:
+        return 1
+
+
+def checkUser(cursor,username):
+    sql = "select * from session where username = '%s'"%username
+    check = cursor.execute(sql)
+    if check == 1L:
+        print "Already a session is created for the user."
+        return 0
+    else:
+        return 1
+
+
+def createSession(db,cursor,session_id,username):
+    sql = ("insert into session values ('%s','%s')")%(session_id,username)
+    try:
+        cursor.execute(sql)
+        db.commit()
+        return 1
+    except:
+        db.rollback()
+        print ("Not able to start the session.")
+        return 0
+
 
 def closeDB(db):
     db.close()
@@ -40,7 +92,7 @@ if __name__=="__main__":
     db,cursor = connectDB()
     if db:
         username,password = fetchDetails()
-        login(cursor,username,password)
+        login(db,cursor,username,password)
         closeDB(db)
     else:
         print ("Not able to connect to the database")
