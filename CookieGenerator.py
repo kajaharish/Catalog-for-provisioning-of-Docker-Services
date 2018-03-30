efbdcde45a2a18dd1c6d6a9169ded70f70d04593
Gaurav Gandhi <grv.gandhi.27@gmail.com>
2018-03-30 22:57:50 +0530

    CookieGenerator.py is added so that it can create cookies on the browser.

--- CookieGenerator.py
+++ CookieGenerator.py
@@ -1,7 +1,14 @@
+#!/usr/bin/python
+
 import Cookie as coo
+import Cookie, datetime, uuid
 
 def genCookie(sessionId):
     cookie = coo.SimpleCookie()
     cookie["docker_session"] = sessionId
+    expires = datetime.datetime.utcnow() + datetime.timedelta(days=10*365) # expires in 30 days
+    cookie['docker_session']['expires'] = expires.strftime("%a, %d %b %Y %H:%M:%S GMT")
+
     print cookie
 
+genCookie("gaurav")
