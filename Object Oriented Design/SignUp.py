780b6215ec04d1f0b0554ccba375f2814ff11141
Gaurav Gandhi <grv.gandhi.27@gmail.com>
2018-05-17 03:43:16 +0530

    SignUp.py is added to the user. It contains the class which can be used for adding new users to the database.y

--- Object Oriented Design/SignUp.py
+++ Object Oriented Design/SignUp.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python2
+
 from User import User
+
 import cgi,cgitb
 
 class SignUp:
 
     def __init__(self):
-        self.user = None
+        self.user= User()
         self.username = None
         self.firstName = None
         self.lastName = None
@@ -16,14 +19,14 @@ class SignUp:
 
     def fetchSignupDetails(self):
         form = cgi.FieldStorage()
-        self.username = form.getvalue('username')
-        self.firstName=form.getvalue('fname')
-        self.lastName=form.getvalue('lname')
-        self.password = form.getvalue('password')
-        self.address = form.getvalue('address')
-        self.question = form.getvalue('ques')
-        self.answer = form.getvalue('answer')
-        self.email = form.getvalue('email')
+        self.user.username = form.getvalue('username')
+        self.user.firstName=form.getvalue('fname')
+        self.user.lastName=form.getvalue('lname')
+        self.user.password = form.getvalue('password')
+        self.user.address = form.getvalue('address')
+        self.user.question = form.getvalue('ques')
+        self.user.answer = form.getvalue('answer')
+        self.user.email = form.getvalue('email')
 
     
     def redirectToLogin(self):
@@ -35,6 +38,22 @@ class SignUp:
     
 
 if __name__ == "__main__":
-    
-    signup = SignUp()
 
+
+    print "Content-type:text/html\r\n\r\n"
+    signup = SignUp()
+    
+    signup.fetchSignupDetails()
+    
+    check = signup.user.isUserExisting()
+
+    if check == 1:
+        checkAddUser = signup.user.addUser()
+        if checkAddUser == 1:
+            checkDatabaseUser = signup.user.createDatabaseUser()
+            if checkDatabaseUser == 0:
+                a = signup.user.deleteUser()
+            else:
+                signup.redirectToLogin()
+    else:
+        print ("Username or email already exists")
